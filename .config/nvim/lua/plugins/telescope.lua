6d6c5cdcfd27e0e9249efdf57bd6d7520f09c670
sjkchang <sjkchang@gmail.com>
2025-02-15 02:13:26 -0800

    More nvim config updates

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -56,12 +56,17 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+          file_ignore_patterns = { 'node_modules', '.git', 'build', 'dist', 'package_lock.json', '.gitignore' },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
--- .config/nvim/lua/plugins/ui/dressing.lua
+++ .config/nvim/lua/plugins/ui/dressing.lua
@@ -1,4 +1,39 @@
+-- Select and input UI.
 return {
-  'stevearc/dressing.nvim',
-  opts = {},
+  {
+    'stevearc/dressing.nvim',
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.input(...)
+      end
+    end,
+    config = function()
+      require('dressing').setup {
+        input = {
+          win_options = {
+            -- Use a purple-ish border.
+            winhighlight = 'FloatBorder:LspFloatWinBorder',
+          },
+        },
+        select = {
+          -- Use a relative window with code actions.
+          get_config = function(opts)
+            if opts.kind == 'codeaction' then
+              return {
+                telescope = require('telescope.themes').get_cursor(),
+              }
+            end
+          end,
+        },
+      }
+    end,
+  },
 }
